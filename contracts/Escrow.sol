6cf97dfd5e675b9ecf9e8c4edda28be1330d0a03
marvelmikel <marvelmikel1997@gmail.com>
2022-11-05 17:24:48 -0700

    Escrow and RealEstate smartcontract tested

---
+++ contracts/Escrow.sol
@@ -0,0 +1,56 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+interface IERC721 {
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _id
+    ) external;
+}
+
+contract Escrow {
+    //state variables
+    address public nftAddress;
+    address payable public seller;
+    address public inspector;
+    address public lender;
+
+    mapping (uint256 => bool) public isListed;
+    mapping (uint256 => uint256) public purchasePrice;
+    mapping (uint256 => uint256) public escrowAmount;
+    mapping (uint256 => address) public buyer;
+
+    constructor(
+        address _nftAddress,
+        address payable _seller,
+        address _inspector,
+        address _lender
+        ){
+        nftAddress = _nftAddress;
+        seller = _seller;
+        inspector = _inspector;
+        lender = _lender;
+
+
+    }
+
+    function list(
+        uint256 _nftID,
+        address _buyer,
+        uint256 _purchasePrice,
+        uint256 _escrowAmount
+    ) public {
+        //Transfer NFT from seller to this contract 
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+
+        isListed[_nftID] = true;
+        purchasePrice[_nftID] = _purchasePrice;
+        escrowAmount[_nftID] = _escrowAmount;
+        buyer[_nftID] = _buyer;
+
+
+    }
+   
+
+}
